66dfbbc627b6c0fe33997b0fe62b9c9c35bddc47
Juan Camilo Astudillo <juanc.astudilloo@uqvirtual.edu.co>
2025-09-09 22:31:21 -0500

    Cambios a la seguridad

--- src/main/java/com/example/usermanagement/controller/UserController.java
+++ src/main/java/com/example/usermanagement/controller/UserController.java
@@ -3,6 +3,8 @@ package com.example.usermanagement.controller;
 import com.example.usermanagement.dto.RegisterRequest;
 import com.example.usermanagement.dto.UserResponse;
 import com.example.usermanagement.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -19,25 +21,29 @@ public class UserController {
     private UserService userService;
 
     @GetMapping
-    @PreAuthorize("hasRole('ADMIN')")
+    //@PreAuthorize("hasRole('ADMIN')")
+    @Operation(security = @SecurityRequirement(name = "bearerAuth"))
     public Page<UserResponse> listUsers(Pageable pageable) {
         return userService.getUsers(pageable);
     }
 
     @GetMapping("/{id}")
     //@PreAuthorize("hasRole('ADMIN')")
+    @Operation(security = @SecurityRequirement(name = "bearerAuth"))
     public UserResponse getUser(@PathVariable Long id) {
         return userService.getUser(id);
     }
 
     @PutMapping("/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
+    //@PreAuthorize("hasRole('ADMIN')")
+    @Operation(security = @SecurityRequirement(name = "bearerAuth"))
     public UserResponse updateUser(@PathVariable Long id, @RequestBody RegisterRequest request) {
         return userService.updateUser(id, request);
     }
 
     @DeleteMapping("/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
+    //@PreAuthorize("hasRole('ADMIN')")
+    @Operation(security = @SecurityRequirement(name = "bearerAuth"))
     public void deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
     }
