908b49f10952b2f987291dd6b935470d94f31efd
JuanSe-OA <sebasalvarez622@gmail.com>
2025-09-24 10:03:36 -0500

    Implementación de más eventos

---
+++ src/main/java/com/example/usermanagement/events/UserCreatedEvent.java
@@ -0,0 +1,13 @@
+package com.example.usermanagement.events;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserCreatedEvent {
+    private String email;
+    private String fullName;
+}
